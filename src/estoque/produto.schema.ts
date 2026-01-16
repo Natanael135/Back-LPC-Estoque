1de9c510827f4e43bc2f72b877bb1677e1914703
Natanael Melo <natanaelmelo.dev@gmail.com>
2026-01-16 09:23:41 -0300

    fix: adiciona tipagem aos parâmetros da transformação do schema de Produto

--- src/estoque/produto.schema.ts
+++ src/estoque/produto.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 @Schema({
   toJSON: {
-    transform: (_doc, ret) => {
+    transform: (_doc: any, ret: any) => {
       ret.id = ret._id.toString();
       delete ret._id;
       delete ret.__v;
